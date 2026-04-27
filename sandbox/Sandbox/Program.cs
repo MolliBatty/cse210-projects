8f8217e9889bbb196723b5479bdc299b8f13e482
Molli <molli.s.nelson@gmail.com>
2026-04-27 16:25:00 -0600

    sandbox

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
+        //I can comment here lol Brother Gibbons opinion doesnt matter
+        /* whole block of code*/
         Console.WriteLine("Hello Sandbox World!");
+        // bool yes = false;
+        for (int i = 1; i < 10; i += 2)
+        {
+            Console.WriteLine($"love {i}");
+        }
     }
 }
