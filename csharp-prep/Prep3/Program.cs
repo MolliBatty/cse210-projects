02652cae7cffb98fb7c5f09775f34dbb92038229
Molli <molli.s.nelson@gmail.com>
2026-04-27 12:08:10 -0600

    Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicnumberInt = randomGenerator.Next(1,101);
+
+        Console.Write("What is your guess? ");
+        string guess = Console.ReadLine();
+
+       int guessInt = int.Parse(guess);
+
+        while (guessInt != magicnumberInt)
+        {
+            if (guessInt > magicnumberInt)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("Higher");
+            }
+    
+            Console.Write("What is your guess? ");
+            guess = Console.ReadLine();
+            guessInt = int.Parse(guess);
+        }
+        Console.WriteLine("You guessed it!");
     }
 }
